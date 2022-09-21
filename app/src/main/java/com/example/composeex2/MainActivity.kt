2d1b110e1f1aad33e7bb3b72841c891f3a120486
finog <finogenov.07@mail.ru>
2022-09-21 22:08:28 +0300

    Added Start screen

--- app/src/main/java/com/example/composeex2/MainActivity.kt
+++ app/src/main/java/com/example/composeex2/MainActivity.kt
@@ -6,9 +6,11 @@ import android.os.Bundle
 import androidx.compose.ui.graphics.Color.Companion.Gray
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.spring
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.horizontalScroll
@@ -26,6 +28,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Color.Companion.Blue
 import androidx.compose.ui.graphics.Color.Companion.Gray
@@ -33,6 +36,7 @@ import androidx.compose.ui.graphics.Color.Companion.Green
 import androidx.compose.ui.graphics.Color.Companion.Yellow
 import androidx.compose.ui.graphics.Outline
 import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.modifier.modifierLocalOf
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -48,73 +52,44 @@ class MainActivity : ComponentActivity() {
 
         setContent {
             ComposeEx2Theme {
-
+                MyApp()
                 // A surface container using the 'background' color from the theme
-                LazyColumn () {
-                    items(100) {
-
-                        CardWithMultipleViews("$it") // it - индекс, поскольку это единственный аргумент лямбды
-                    }
-                }
 
 
-                }
-
-                }
             }
+
         }
-
-
-
-
-@Composable
-fun CardWithMultipleViews(string: String) {
-
-    Modifier.padding(10.dp)
-    var hidden by remember{ mutableStateOf(true) }
-    val modOne = Modifier
-        .padding(10.dp)
-        .fillMaxWidth()
-    val modTwo = Modifier
-        .padding(10.dp)
-        .clip(RoundedCornerShape(10.dp))
-
-
-    Card(
-        elevation = 40.dp,
-        modifier = modTwo,
-    )
-     {
-        Column(modifier = modOne, horizontalAlignment = Alignment.CenterHorizontally) {
-            Row(modifier = modOne, horizontalArrangement = Arrangement.SpaceBetween) {
-                Column() {
-                    Text(text = "Hello,", fontSize = 18.sp)
-                    Text(text = "$string", fontSize = 42.sp, style = MaterialTheme.typography.h4, fontWeight = FontWeight.Bold)
-                }
-                Button(onClick = { hidden = !hidden } ){
-                    if (hidden == true){Text(text = "Show More", fontSize = 14.sp)}
-
-                    if (hidden == false) {Text(text = "Show Less", fontSize = 14.sp)}
-                }
-
-            }
-            if (!hidden){
-
-                Text(text = "Lorem ipsum dolor sit amet, consectetur " +
-                        "adipiscing elit. Ut lectus elit, varius ac felis at, congue pulvinar" +
-                        " lorem. Curabitur fringilla faucibus laoreet. " +
-                        "Proin vitae augue.", fontSize = 16.sp)
-            }
-        }
-
     }
 }
 
 
 @Composable
-fun OnboardingScreen() {
-    // TODO: Это состояние должно быть поднято
-    var shouldShow by remember { mutableStateOf(true) }
+fun LoopCard() {
+    LazyColumn() {
+        items(100) {
+
+            CardWithMultipleViews("$it") // it - индекс, поскольку это единственный аргумент лямбды
+        }
+    }
+}
+@Composable
+fun MyApp() {
+
+    var shouldShowOnboarding by remember { mutableStateOf(true) }
+
+    if (shouldShowOnboarding) {
+        OnboardingScreen(onContinueClicked = { shouldShowOnboarding = false })
+    } else {
+        LazyColumn() {
+            items(100) {
+
+                CardWithMultipleViews("$it") // it - индекс, поскольку это единственный аргумент лямбды
+            }
+        }
+    }
+}
+@Composable
+fun OnboardingScreen(onContinueClicked: () -> Unit) {
 
     Surface {
         Column(
@@ -125,7 +100,7 @@ fun OnboardingScreen() {
             Text("Welcome to the Basics Codelab!")
             Button(
                 modifier = Modifier.padding(vertical = 24.dp),
-                onClick = { shouldShow = false }
+                onClick = onContinueClicked
             ) {
                 Text("Continue")
             }
@@ -133,16 +108,72 @@ fun OnboardingScreen() {
     }
 }
 
-
-
-@Preview(showBackground = true, widthDp = 320, heightDp = 320)
 @Composable
-fun OnboardingPreview() {
-    ComposeEx2Theme() {
-        OnboardingScreen()
+fun CardWithMultipleViews(string: String) {
+
+    Modifier.padding(10.dp)
+    var hidden by rememberSaveable { mutableStateOf(true) }
+    val modBut = Modifier
+        .padding(horizontal = 5.dp)
+    val modRow = Modifier
+        .fillMaxWidth()
+    val modText = Modifier
+        .padding(vertical = 5.dp, horizontal = 10.dp)
+    val modOne = Modifier
+        .padding(10.dp)
+        .fillMaxWidth()
+    val modTwo = Modifier
+        .padding(horizontal = 10.dp, vertical = 5.dp)
+        .clip(RoundedCornerShape(16.dp))
+
+    val colorcard = rgb(228, 228, 228)
+    Card(
+        elevation = 20.dp,
+        modifier = modTwo
+    )
+    {
+        Surface(color = /*colorcard*/Color.LightGray) {
+            Column(modifier = modOne, horizontalAlignment = Alignment.CenterHorizontally) {
+                Row(modifier = modRow, horizontalArrangement = Arrangement.SpaceBetween) {
+                    Column() {
+                        Text(text = "Hello,", fontSize = 18.sp, modifier = modText)
+                        Text(
+                            text = "$string",
+                            fontSize = 42.sp,
+                            style = MaterialTheme.typography.h4,
+                            fontWeight = FontWeight.Bold,
+                            modifier = modText
+                        )
+                    }
+                    Button(onClick = { hidden = !hidden }, modifier = modBut) {
+                        if (hidden == true) {
+                            Text(text = "Show More", fontSize = 14.sp)
+                        }
+
+                        if (hidden == false) {
+                            Text(text = "Show Less", fontSize = 14.sp)
+                        }
+                    }
+
+                }
+                AnimatedVisibility(!hidden) {
+
+                    Text(
+                        text = "Lorem ipsum dolor sit amet, consectetur " +
+                                "adipiscing elit. Ut lectus elit, varius ac felis at, congue pulvinar" +
+                                " lorem. Curabitur fringilla faucibus laoreet. " +
+                                "Proin vitae augue.", fontSize = 16.sp
+                    )
+                }
+            }
+        }
+
+
     }
-    
 }
+
+
+
 
 
 
