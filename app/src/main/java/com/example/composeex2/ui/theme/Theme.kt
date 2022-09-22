d8eb9c190e479749b58ae4534bfde39899f2b4f2
finog <finogenov.07@mail.ru>
2022-09-22 08:09:26 +0300

    Added New Theme, and last them has be changed

--- app/src/main/java/com/example/composeex2/ui/theme/Theme.kt
+++ app/src/main/java/com/example/composeex2/ui/theme/Theme.kt
@@ -5,26 +5,30 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 
-private val DarkColorPalette = darkColors(
+/*private val DarkColorPalette = darkColors(
     primary = Purple200,
     primaryVariant = Purple700,
     secondary = Teal200
+)*/
+
+private val DarkColorPalette = darkColors(
+    surface = Color.Black,
+    onSurface = Color.Green,
+    primary = Purple700,
+    onPrimary = Color.White
+
 )
 
-private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200
 
-    /* Other default colors to override
-    background = Color.White,
-    surface = Color.White,
-    onPrimary = Color.White,
-    onSecondary = Color.Black,
-    onBackground = Color.Black,
-    onSurface = Color.Black,
-    */
+
+
+private val LightColorPalette = lightColors(
+    surface = LightBlue,
+    onSurface = Color.White,
+    primary = ButtonSky,
+    onPrimary = Navy
 )
 
 @Composable
